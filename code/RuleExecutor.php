24022f43c3820a41d9ae42404b11a5d9fa6a97e0
Ariex <ariexcorn@gmail.com>
2015-07-23 15:24:26 +0930

    extend rule executor to make it support extend from outside update rule executor to support array

--- code/RuleExecutor.php
+++ code/RuleExecutor.php
@@ -1,8 +1,161 @@
 <?php
 
+interface IOperatable {
+	public function Equals($right);
+	public function GreaterThan($right);
+	public function EqualsOrGreaterThan($right);
+	public function LessThan($right);
+	public function EqualsOrLessThan($right);
+	public function StartsWith($right);
+	public function EndsWith($right);
+	public function Contains($right);
+}
+
+interface IExtendFunctionSupportable {
+	public function length();
+}
+
+interface IRuleNodeFactory {
+	public function CreateNode($rawValue);
+}
+
+abstract class RuleNode extends Object implements IOperatable, IExtendFunctionSupportable {
+	protected $RawValue;
+
+	protected function __construct($rawValue) {
+		$this->RawValue = $rawValue;
+	}
+
+	public static function CreateNode($rawValue) {
+		if (is_bool($rawValue)) {
+			$rawValue = $rawValue ? "True" : "False";
+		}
+		if (is_string($rawValue)) {
+			return StringRuleNode::create($rawValue);
+		} else if (is_array($rawValue)) {
+			return ArrayRuleNode::create($rawValue);
+		}
+		return null;
+	}
+
+	public abstract function getNodeType();
+
+	public function getRawValue() {
+		return $this->RawValue;
+	}
+
+	/* operators */
+	public function Equals($right) {
+		throw new Exception($this->getNodeType() . ' does not support this operation - GreaterThan.');
+	}
+
+	public function GreaterThan($right) {
+		throw new Exception($this->getNodeType() . ' does not support this operation - GreaterThan.');
+	}
+
+	public function EqualsOrGreaterThan($right) {
+		throw new Exception($this->getNodeType() . ' does not support this operation - EqualsOrGreaterThan.');
+	}
+
+	public function LessThan($right) {
+		throw new Exception($this->getNodeType() . ' does not support this operation - LessThan.');
+	}
+
+	public function EqualsOrLessThan($right) {
+		throw new Exception($this->getNodeType() . ' does not support this operation - EqualsOrLessThan.');
+	}
+
+	public function StartsWith($right) {
+		throw new Exception($this->getNodeType() . ' does not support this operation - StartsWith.');
+	}
+
+	public function EndsWith($right) {
+		throw new Exception($this->getNodeType() . ' does not support this operation - EndsWith.');
+	}
+
+	public function Contains($right) {
+		throw new Exception($this->getNodeType() . ' does not support this operation - GreaterThan.');
+	}
+
+	/* additional functions can be accessed after a dot */
+	public function length() {
+		throw new Exception($this->getNodeType() . ' does not support this method - length.');
+	}
+}
+
+class StringRuleNode extends RuleNode {
+	public function getNodeType() {
+		return 'String';
+	}
+
+	public function Equals($right) {
+		return $this->RawValue == (string) $right;
+	}
+
+	public function GreaterThan($right) {
+		$l = floatval($this->RawValue);
+		$r = floatval($right);
+		return $l > $r;
+	}
+
+	public function EqualsOrGreaterThan($right) {
+		$l = floatval($this->RawValue);
+		$r = floatval($right);
+		return $l >= $r;
+	}
+
+	public function LessThan($right) {
+		$l = floatval($this->RawValue);
+		$r = floatval($right);
+		return $l < $r;
+	}
+
+	public function EqualsOrLessThan($right) {
+		$l = floatval($this->RawValue);
+		$r = floatval($right);
+		return $l <= $r;
+	}
+
+	public function StartsWith($right) {
+		return substr($this->RawValue, 0, strlen($right)) == $right;
+	}
+
+	public function EndsWith($right) {
+		return substr($this->RawValue, -strlen($right)) == $right;
+	}
+
+	public function Contains($right) {
+		return strpos($this->RawValue, $right) !== false;
+	}
+
+	/* additional functions can be accessed after a dot */
+	public function length() {
+		return strlen($this->RawValue);
+	}
+}
+
+class ArrayRuleNode extends RuleNode {
+	public function getNodeType() {
+		return 'Array';
+	}
+
+	public function Equals($right) {
+		return in_array($right, $this->RawValue);
+	}
+
+	public function Contains($right) {
+		return $this->Equals($right);
+	}
+
+	/* additional functions can be accessed after a dot */
+	public function length() {
+		return sizeof($this->RawValue);
+	}
+}
+
 class RuleExecutor extends Object {
-	public function Execute($statement, $fields) {
-		r("orig statement {$statement}");
+	public function Execute($statement, $fields, ...$nodeFactories) {
+		// r("orig statement {$statement}");
 		if (strlen($statement) < 1) {
 			return true;
 		}
@@ -12,50 +165,79 @@ class RuleExecutor extends Object {
 		$mres            = $matches ? $matches : array();
 		$lenMatchedRules = sizeof($mres[0]);
 		$lefts           = $mres[1];
-		r($mres);
+		// r($mres);
 		$funcMap = array();
 		for ($i = 0; $i < sizeof($lefts); $i++) {
 			$parts    = preg_split('/\./', $lefts[$i]);
 			$realLeft = $parts[0];
-			r("Field {$realLeft} exist");
+			// r("Field {$realLeft} exist");
 			$f = $fields->dataFieldByName($realLeft);
 			if ($f == null) {
 				throw new Exception("Field {$realLeft} does not exist");
 			}
-			$opt   = $mres[2][$i];
-			$left  = strtolower((string) $f->Value());
-			$right = strtolower($mres[3][$i]);
-			r($opt);
-			$func = function () use ($opt, $left, $right, $parts) {
-				if (sizeof($parts) == 2) {
-					switch (strtolower($parts[1])) {
-						case "length":
-							$left = strlen($left);
-							break;
-						default:
-							$left = $left;
+			$opt = $mres[2][$i];
+			$val = $f->Value();
+			// if (!is_string($val)) {
+			// 	return true;
+			// }
+			// different type of controls have different type of value
+			// for textboxfield it is just a string, bot checkboxsetfield, it is an array
+			$left = RuleNode::CreateNode($val);
+			if ($left == null && $nodeFactories != null) {
+				foreach ($nodeFactories as $factory) {
+					if (!($factory instanceof IRuleNodeFactory)) {
+						continue;
+					}
+					$left = $factory->CreateNode($val);
+					if ($left != null) {
+						break;
 					}
 				}
-				$left  = strtolower($left);
-				$right = strtolower($right);
-				r("{$left} {$opt} {$right}");
+			}
+			if ($left == null) {
+				throw new Exception('Unsupported value at left.');
+			}
+			// $left  = strtolower((string) $val);
+			$right = strtolower($mres[3][$i]);
+			// r($opt);
+			$func = function () use ($opt, $left, $right, $parts) {
+				if (sizeof($parts) == 2) {
+					if (!($left instanceof IExtendFunctionSupportable)) {
+						throw new Exception('$Left does not support extension method');
+					}
+					switch (strtolower($parts[1])) {
+						case "length":
+							$left = RuleNode::CreateNode($left->length());
+							break;
+						default:
+							// do nothing at here...
+					}
+				}
+				// $left  = strtolower($left);
+				// $right = strtolower($right);
+				// r("{$left} {$opt} {$right}");
+				if (!($left instanceof IOperatable)) {
+					throw new Exception('$Left is not a supported type.');
+				}
 				switch ($opt) {
 					case "==":
-						return $left == $right;
+						return $left->Equals($right);
 					case ">":
-						return floatval($left) > floatval($right);
+						return $left->GreaterThan($right);
 					case "<":
-						return floatval($left) < floatval($right);
+						return $left->LessThan($right);
 					case ">=":
-						return floatval($left) >= floatval($right);
+						return $left->EqualsOrGreaterThan($right);
 					case "<=":
-						return floatval($left) <= floatval($right);
+						return $left->EqualsOrLessThan($right);
 					case "!=":
-						return $left != $right;
+						return !$left->Equals($right);
 					case "^=":
-						return substr($left, 0, strlen($right)) == $right;
+						return $left->StartsWith($right);
 					case "$=":
-						return substr($left, -strlen($right)) == $right;
+						return $left->EndsWith($right);
+					case "@=":
+						return $left->Contains($right);
 					default:
 						throw new Exception("Not supported operation {$opt}.");
 				}
@@ -68,22 +250,22 @@ class RuleExecutor extends Object {
 				'Func'  => $func,
 			));
 		}
-		r($funcMap);
+		// r($funcMap);
 		for ($i = 0; $i < sizeof($funcMap); $i++) {
 			$statement = str_replace($funcMap[$i]['Key'], "{$funcMap[$i]['Alias']}()", $statement);
 		}
-		r("statement used to create function $statement");
+		// r("statement used to create function $statement");
 		$funcs = array_map(function ($val) {
 			return $val['Func'];
 		}, $funcMap);
-		r($funcs);
-		r(implode(',', array_map(function ($v) {
-			return $v['Alias'];
-		}, $funcMap)));
+		// r($funcs);
+		// r(implode(',', array_map(function ($v) {
+		// 	return $v['Alias'];
+		// }, $funcMap)));
 		$stateFunc = create_function(implode(',', array_map(function ($v) {
 			return $v['Alias'];
 		}, $funcMap)), 'return ' . $statement . ';');
-		r($stateFunc);
+		// r($stateFunc);
 		return call_user_func_array($stateFunc, $funcs);
 	}
 }
